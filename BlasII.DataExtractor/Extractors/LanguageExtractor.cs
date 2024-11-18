ba648a9572b7dc75d8632d7a5df6a2d5c918da16
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-11-18 10:18:53 -0500

    lang: save processed data to file

--- BlasII.DataExtractor/Extractors/LanguageExtractor.cs
+++ BlasII.DataExtractor/Extractors/LanguageExtractor.cs
@@ -1,5 +1,8 @@
 ﻿using BlasII.ModdingAPI;
+using BlasII.ModdingAPI.Helpers;
 using Il2CppI2.Loc;
+using System.IO;
+using System.Text;
 
 namespace BlasII.DataExtractor.Extractors;
 
@@ -37,10 +40,15 @@ internal class LanguageExtractor : IExtractor
         PrintPercentMessage($"{name}/{code}");
 
         int langIdx = source.GetLanguageIndexFromCode(code);
+        var sb = new StringBuilder();
+
         foreach (string term in source.GetTermsList())
         {
-            ModLog.Warn($"{term}: {source.GetTermData(term).Languages[langIdx]}");
+            string value = source.GetTermData(term).Languages[langIdx].Trim().Replace('\n', '@');
+            sb.AppendLine($"{term}: {value}");
         }
+
+        SaveToFile(Path.Combine(name, $"{code}.txt"), sb.ToString());
     }
 
     private void CalculateTotal()
@@ -61,6 +69,15 @@ internal class LanguageExtractor : IExtractor
     {
         int percent = (int)((float)++_current / _total * 100);
         ModLog.Info($"[{percent}%] Extracting {name}");
+    }
+
+    private void SaveToFile(string path, string contents)
+    {
+        path = Path.Combine(Main.DataExtractor.FileHandler.ContentFolder, Name, VersionHelper.GameVersion, path);
+        Directory.CreateDirectory(Path.GetDirectoryName(path));
+
+        //ModLog.Warn($"Writing extracted data to {path}");
+        File.WriteAllText(path, contents);
     }
 
     private int _current;
