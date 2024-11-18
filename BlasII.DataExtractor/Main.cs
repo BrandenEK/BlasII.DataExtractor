03acbab415b746f41eeb8827c26e2d4c9a4ddb40
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-11-18 07:41:20 -0500

    init: add base project files

---
+++ BlasII.DataExtractor/Main.cs
@@ -0,0 +1,13 @@
+﻿using MelonLoader;
+
+namespace BlasII.DataExtractor;
+
+internal class Main : MelonMod
+{
+    public static DataExtractor DataExtractor { get; private set; }
+
+    public override void OnLateInitializeMelon()
+    {
+        DataExtractor = new DataExtractor();
+    }
+}
---
+++ BlasII.DataExtractor/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using BlasII.DataExtractor;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(BlasII.DataExtractor.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
+[assembly: MelonOptionalDependencies("Modding API", "0.1.0")]
