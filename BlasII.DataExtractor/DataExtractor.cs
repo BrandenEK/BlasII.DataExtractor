4580f8e25c32b2abae2d68a6d165280095c4490a
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-11-18 07:55:14 -0500

    core: finish setup

--- BlasII.DataExtractor/DataExtractor.cs
+++ BlasII.DataExtractor/DataExtractor.cs
@@ -2,6 +2,9 @@
 
 namespace BlasII.DataExtractor;
 
+/// <summary>
+/// Adds methods of extracting data from different versions of the game
+/// </summary>
 public class DataExtractor : BlasIIMod
 {
     internal DataExtractor() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
--- BlasII.DataExtractor/Properties/AssemblyInfo.cs
+++ BlasII.DataExtractor/Properties/AssemblyInfo.cs
@@ -2,4 +2,5 @@ using BlasII.DataExtractor;
 using MelonLoader;
 
 [assembly: MelonInfo(typeof(BlasII.DataExtractor.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
-[assembly: MelonOptionalDependencies("Modding API", "0.1.0")]
+[assembly: MelonOptionalDependencies("Modding API", "1.1.0", "Cheat Console", "1.2.0")]
+[assembly: MelonPriority(1000)] // Temp until better priority sorting is added
