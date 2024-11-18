44d41cd0d6dca4de8d5266726da74276a44d9c0c
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-11-18 10:03:41 -0500

    lang: search through every language and source

--- BlasII.DataExtractor/Extractors/LanguageExtractor.cs
+++ BlasII.DataExtractor/Extractors/LanguageExtractor.cs
@@ -1,4 +1,7 @@
-﻿namespace BlasII.DataExtractor.Extractors;
+﻿using BlasII.ModdingAPI;
+using Il2CppI2.Loc;
+
+namespace BlasII.DataExtractor.Extractors;
 
 internal class LanguageExtractor : IExtractor
 {
@@ -6,6 +9,60 @@ internal class LanguageExtractor : IExtractor
 
     public void Extract()
     {
+        CalculateTotal();
 
+        int sourceIdx = 0;
+        foreach (LanguageSourceData source in LocalizationManager.Sources)
+        {
+            if (source.GetTermsList().Count == 0)
+                continue;
+
+            ExtractSource($"source{sourceIdx}", source);
+            sourceIdx++;
+        }
     }
+
+    private void ExtractSource(string name, LanguageSourceData source)
+    {
+        PrintPercentMessage(name);
+
+        foreach (string code in source.GetLanguagesCode())
+        {
+            ExtractLanguage(name, code, source);
+        }
+    }
+
+    private void ExtractLanguage(string name, string code, LanguageSourceData source)
+    {
+        PrintPercentMessage($"{name}/{code}");
+
+        int langIdx = source.GetLanguageIndexFromCode(code);
+        foreach (string term in source.GetTermsList())
+        {
+            ModLog.Warn($"{term}: {source.GetTermData(term).Languages[langIdx]}");
+        }
+    }
+
+    private void CalculateTotal()
+    {
+        _current = 0;
+        _total = 0;
+
+        foreach (LanguageSourceData source in LocalizationManager.Sources)
+        {
+            if (source.GetTermsList().Count == 0)
+                continue;
+
+            _total += source.GetLanguagesCode().Count + 1;
+        }
+    }
+
+    private void PrintPercentMessage(string name)
+    {
+        int percent = (int)((float)++_current / _total * 100);
+        ModLog.Info($"[{percent}%] Extracting {name}");
+    }
+
+    private int _current;
+    private int _total;
 }
