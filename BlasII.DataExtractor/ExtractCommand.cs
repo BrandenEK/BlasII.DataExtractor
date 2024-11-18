8c20b06b87f6771f0e2816d3748d8189a4378bde
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-11-18 08:02:41 -0500

    core: register extract command

---
+++ BlasII.DataExtractor/ExtractCommand.cs
@@ -0,0 +1,22 @@
+﻿using BlasII.CheatConsole;
+
+namespace BlasII.DataExtractor;
+
+/// <summary>
+/// Handles extracting all kinds of game data
+/// </summary>
+public class ExtractCommand : ModCommand
+{
+    /// <summary>
+    /// Creates a new extract command
+    /// </summary>
+    public ExtractCommand() : base("extract") { }
+
+    /// <summary>
+    /// Extracts a certain kind of data
+    /// </summary>
+    public override void Execute(string[] args)
+    {
+        Write("Extracting...");
+    }
+}
